3346d84996bdf989e1e9f51d0db47b1a1aa3dc15
Kitsune Solar <kitsune.solar@gmail.com>
2017-01-08 15:58:35 +0300

    Update resource

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ use Flarum\Event\ConfigureLocales;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
-  $events->listen(ConfigureLocales::class, function (ConfigureLocales $event) {
-    $event->loadLanguagePackFrom(__DIR__);
-  });
+    $events->listen(ConfigureLocales::class, function (ConfigureLocales $event) {
+        $event->loadLanguagePackFrom(__DIR__);
+    });
 };
